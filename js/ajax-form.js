8ee72b10e3f5a8c062572ee97fccd535c06fad98
Solomon <programmingacct007@gmail.com>
2024-09-16 15:52:44 +0100

    fixed EmailJs

--- js/ajax-form.js
+++ js/ajax-form.js
@@ -55,7 +55,7 @@
 	emailjs.init("NJqzIt6s0gaIVYhTD"); 
   })();
   
-  document.getElementById('contact-form').addEventListener('submit', function(event) {
+  document.getElementById('form').addEventListener('submit', function(event) {
 	event.preventDefault(); // Prevent the default form submission
   
 	// Get form data
@@ -63,12 +63,12 @@
 	var formData = new FormData(form);
   
 	// Send email using EmailJS
-	emailjs.sendForm("service_solomon","template_nq2fcdl", formData)
+	emailjs.sendForm("service_solomon","template_nq2fcdl", this)
 	  .then(function(response) {
-		console.log('Email sent successfully:', response);
+		alert('Email sent successfully:', response);
 		// Optionally, show a success message to the user
 	  }, function(error) {
-		console.log('Error sending email:', error);
+		alert('Error sending email:', error);
 		// Optionally, show an error message to the user
 	  });
   });
